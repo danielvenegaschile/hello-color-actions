0084004864ff724629fd8d5bcc44dd4f80035c67
danielvenegaschile <daniel_venegas@me.com>
2024-03-20 00:54:44 -0300

    prueba

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Hola mundo desde Chile");
+    expect(hello.hello()).toEqual("Hola mundo desde 2Chile");
   });
 });
